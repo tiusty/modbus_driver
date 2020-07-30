bd986f71ebcd08c26b8039ca81d48eed7ed46124
tiusty <awagud12@gmail.com>
2020-07-30 16:29:36 -0400

    Started writing modbus program

--- main.cpp
+++ main.cpp
@@ -3,7 +3,23 @@
 #include <modbus/modbus.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    modbus_t *mb;
+    uint16_t tab_reg[32];
+
+    mb = modbus_new_rtu("\\\\.\\COM5", 9600, 'N', 8, 1);
+    int rc = modbus_set_slave(mb, 1);
+    std::cout << "Setting slave ID " << rc << std::endl;
+    if (modbus_connect(mb) == -1) {
+        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+    } else {
+        std::cout << "Connected!" << std::endl;
+        /* Read 5 registers from the address 0 */
+        modbus_write_register(mb, 40001, 1);
+        std::cout << "Wrote to register!" << std::endl;
+    }
+
+    modbus_close(mb);
+    modbus_free(mb);
     return 0;
 }
 
