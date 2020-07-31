215828c7f8c14fbffc5e41279956c86dfa75acd3
tiusty <awagud12@gmail.com>
2020-07-30 23:05:37 -0400

    It is now communicating with the Arudino!

--- main.cpp
+++ main.cpp
@@ -6,16 +6,31 @@ int main() {
     modbus_t *mb;
     uint16_t tab_reg[32];
 
-    mb = modbus_new_rtu("\\\\.\\COM5", 9600, 'N', 8, 1);
+    mb = modbus_new_rtu("/dev/ttyS5", 9600, 'N', 8, 1);
+    if (mb == NULL) {
+        fprintf(stderr, "Unable to create the libmodbus context\n");
+        return -1;
+    }
     int rc = modbus_set_slave(mb, 1);
-    std::cout << "Setting slave ID " << rc << std::endl;
+    std::cout << "Setting slave ID, status: " << rc << std::endl;
+    modbus_set_debug(mb, TRUE);
+
+    if (modbus_rtu_set_serial_mode(mb, MODBUS_RTU_RS485) == -1) {
+        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+    }
+    int rc2 = modbus_rtu_get_serial_mode(mb);
+    std::cout << rc2 << std::endl;
     if (modbus_connect(mb) == -1) {
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
     } else {
         std::cout << "Connected!" << std::endl;
         /* Read 5 registers from the address 0 */
-        modbus_write_register(mb, 40001, 1);
-        std::cout << "Wrote to register!" << std::endl;
+
+        if ( modbus_write_register(mb, 0, 1) == -1) {
+            fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+        } else {
+            std::cout << "Wrote to register!" << std::endl;
+        }
     }
 
     modbus_close(mb);
