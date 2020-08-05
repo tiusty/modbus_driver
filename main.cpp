f699f587c7badbe1e2116e59f42c8b97bc9ac01f
tiusty <awagud12@gmail.com>
2020-08-05 16:53:40 -0400

    Fix spelling

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@
 
 int main() {
     // Declare the devices
-    ModbusDevice arudino;
+    ModbusDevice arduino;
     ModbusDevice aqua_troll_500;
 
     // Declare the driver
     ModbusDriver driver;
 
     // Initialize the desired devices
+    driver.initialize_device(arduino, "/dev/ttyS6", "Arudino");
 //    driver.initialize_device(aqua_troll_500, "/dev/ttyS3", "Aqua Troll 500", 12000);
-    driver.initialize_device(arudino, "/dev/ttyS6", "Arudino");
 
     // Write a value to the arduino
-    driver.write(arudino, 0,0);
+    driver.write(arduino, 0,0);
+//    driver.write(aqua_troll_500, 0, 1);
 
 
     return 0;
