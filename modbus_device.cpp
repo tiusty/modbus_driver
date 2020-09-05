b62c9701fc992b53883e3c92155115803b777b84
tiusty <awagud12@gmail.com>
2020-09-05 16:48:51 -0400

    Got it to read the temperature by increasing the timeout time!!!!

--- modbus_device.cpp
+++ modbus_device.cpp
@@ -27,6 +27,21 @@ int ModbusDevice::init(const std::string &device_port, const std::string &device
     uint16_t tab_reg[1];
     modbus_read_registers(mb_, 1, 1, tab_reg);
 
+    modbus_set_debug(mb_, TRUE);
+
+    uint32_t old_response_to_sec;
+    uint32_t old_response_to_usec;
+
+    // This code is for version 3.0.6
+    modbus_get_response_timeout(mb_, &old_response_to_sec, &old_response_to_usec);
+    std::cout << "Old Response. Seconds: " << old_response_to_sec << " msec : " << old_response_to_usec << std::endl;
+
+    /* Define a new and too short timeout! */
+    modbus_set_response_timeout(mb_, 120, 0);
+
+    modbus_get_response_timeout(mb_, &old_response_to_sec, &old_response_to_usec);
+    std::cout << "Old Response. Seconds: " << old_response_to_sec << " msec : " << old_response_to_usec << std::endl;
+
     return 0;
 }
 
@@ -45,18 +60,27 @@ int ModbusDevice::write_to_register(int location, int value)
 
 int ModbusDevice::read_from_register(int address, int number_of_registers)
 {
-    uint16_t tab_reg[64];
-    tab_reg[0] = 0;
-    tab_reg[1] = 0;
+    uint16_t tab_reg[2];
+    for (int i=0; i < 2; i++) {
+        tab_reg[i] = 0;
+    }
 
-    if (modbus_read_registers(mb_, address, number_of_registers, tab_reg) == -1)
+    int rc = modbus_read_registers(mb_, address, number_of_registers, tab_reg);
+
+    if (rc == -1)
     {
         fprintf(stderr, "Modbus read register failed %s: %s\n", device_name_.c_str(),  modbus_strerror(errno));
         return -1;
     }
 
-    std::cout << tab_reg[0] << "return value" << std::endl;
-    std::cout << tab_reg[1] << "return value" << std::endl;
+    for (int i=0; i < rc; i++) {
+        printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
+    }
+
+
+    std::cout << "rc " << rc << std::endl;
+    std::cout << "first: " << tab_reg[0] << std::endl;
+    std::cout << "second: " << tab_reg[1] << std::endl;
 
     return 0;
 }
